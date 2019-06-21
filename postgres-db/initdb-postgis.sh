66deda8009beabcee3b5f5c1e625bd7f95f4cd88
Joaquin Olivera <joaquin.olivera@gmail.com>
2019-06-21 16:26:41 -0400

    osm Paraguay map migrate and osm2pgsql installation

--- postgres-db/initdb-postgis.sh
+++ postgres-db/initdb-postgis.sh
@@ -19,3 +19,18 @@ psql smarttraffic -c "CREATE EXTENSION IF NOT EXISTS postgis;"
 psql smarttraffic -c "CREATE EXTENSION IF NOT EXISTS postgis_topology;"
 psql smarttraffic -c "CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;"
 psql smarttraffic -c "CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;"
+psql smarttraffic -c "CREATE EXTENSION IF NOT EXISTS hstore;"
+
+
+#add osm data to the database...
+osm2pgsql --create --slim \
+    --database smarttraffic \
+    --style /osm/openstreetmap-carto/openstreetmap-carto.style --multi-geometry \
+    /osm/paraguay-latest.osm.pbf
+
+#2 causes 2 CPU cores to be used.
+#    --cache 1000 --number-processes 2 --hstore \
+
+#tells what columns to create with the style
+#create multi-geometry
+#the last line is the path to the map
