d6cc2e2e8b9ae0cd437710464490d756fd8676ad
joaquinolivera <joaquin@Joaquin.localdomain>
2019-08-01 12:31:46 -0400

    add some services for session management of the user in client

--- spusers/urls.py
+++ spusers/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 from rest_framework import routers
-from spusers.views import LoginView, ResetPassView, isLoggedView
+from spusers.views import ChangePasswordView, LoginView, LogoutView, isLoggedView
 
 from . import views
 
@@ -8,8 +8,9 @@ router = routers.DefaultRouter()
 router.register("users", views.UserView)
 
 urlpatterns = [
+    path("changePass/", ChangePasswordView.as_view()),
     path("isUserLogged/", isLoggedView.as_view()),
     path("loggin/", LoginView.as_view()),
-    path("isReseteable/", ResetPassView.as_view()),
+    path("logout/", LogoutView.as_view()),
     path("", include(router.urls)),
 ]
--- spusers/views.py
+++ spusers/views.py
@@ -1,15 +1,15 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_protect
 from rest_framework import status, viewsets
 from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .serializer import UserSerializer
 
-# from rest_framework.permissions import IsAuthenticated
+# from rest_framework.decorators import detail_route
 
 
 class UserView(viewsets.ModelViewSet):
@@ -23,13 +23,27 @@ class isLoggedView(APIView):
     authentication_classes = (SessionAuthentication, TokenAuthentication)
 
     def get(self, request, format=None):
-        if request.user is not None:
-            if request.user.is_authenticated and request.session:
-                return Response(request.session, status=status.HTTP_200_OK)
-            else:
-                return Response(request.session, status=status.HTTP_403_FORBIDDEN)
+        if request.user.is_authenticated:
+            return Response(request.session, status=status.HTTP_200_OK)
         else:
-            return Response(request.session, status=status.HTTP_404_UNAUTHORIZED)
+            return Response(UserSerializer(request.user).data, status=status.HTTP_403_FORBIDDEN)
+
+
+class ChangePasswordView(APIView):
+    authentication_classes = (SessionAuthentication, TokenAuthentication)
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, format=None):
+        data = request.data
+        current_pass = data.get("current_pass", None)
+        new_pass = data.get("new_pass", None)
+        user = authenticate(username=request.user.username, password=current_pass)
+        if user is not None:
+            user.password = make_password(new_pass)
+            user.save()
+            return Response(status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginView(APIView):
@@ -52,22 +66,10 @@ class LoginView(APIView):
         else:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-    @method_decorator(csrf_protect)
-    def get(self, request, format=None):
-        return Response({}, status=status.HTTP_200_OK)
 
-
-class ResetPassView(APIView):
+class LogoutView(APIView):
     authentication_classes = (TokenAuthentication, SessionAuthentication)
 
-    def post(self, request, format=None):
-        try:
-            User.objects.get(
-                username=request.data.get("username"),
-                smartparkingprofile__birth_date=request.data.get("birth_date"),
-                smartparkingprofile__sex=request.data.get("sex"),
-            )
-        except User.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        else:
-            return Response(status=status.HTTP_202_ACCEPTED)
+    def get(self, request, format=None):
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
