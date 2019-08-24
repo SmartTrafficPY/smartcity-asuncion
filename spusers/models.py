e56bf57df02d088756fa81e9a7ff17aae7f192e0
Javier Paniagua <paniagua@spaziodati.eu>
2019-08-24 12:29:44 +0200

    remove alias from smartparking profiles

--- spusers/models.py
+++ spusers/models.py
@@ -11,6 +11,5 @@ class SmartParkingProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birth_date = models.DateField(null=True, blank=True)
     sex = models.CharField(max_length=16, choices=SEX_CHOICES)
-    alias = models.CharField(max_length=50, unique=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
