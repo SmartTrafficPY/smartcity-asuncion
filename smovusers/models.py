5878aaa5fa66763267e29827863369c049c9990e
Javier Paniagua <paniagua@spaziodati.eu>
2019-10-07 21:33:02 +0200

    tweaks here and there to make it work

--- smovusers/models.py
+++ smovusers/models.py
@@ -2,9 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-# Create your models here.
 class MovementType(models.Model):
-
     name = models.CharField(max_length=50)
 
     def __str__(self):
@@ -12,17 +10,16 @@ class MovementType(models.Model):
 
 
 class SmartMovingProfile(models.Model):
-    FEMALE = "F"
-    MALE = "M"
-
-    SEX_CHOICES = ((MALE, "Male"), (FEMALE, "Female"))
+    SEX_FEMALE = "F"
+    SEX_MALE = "M"
+    SEX_CHOICES = ((SEX_MALE, "Male"), (SEX_FEMALE, "Female"))
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birth_date = models.DateField(null=True, blank=True)
     sex = models.CharField(max_length=16, choices=SEX_CHOICES)
-    type_movement = models.ForeignKey(MovementType, on_delete=models.CASCADE)
+    movement_type = models.ForeignKey(MovementType, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.user.username
+        return f"{self.user.username}"
--- smovusers/serializers.py
+++ smovusers/serializers.py
@@ -3,44 +3,40 @@ from django.contrib.auth.models import User
 from django.db import transaction
 from rest_framework import serializers
 
-from .models import MovementType, SmartMovingProfile
+from .models import SmartMovingProfile
 
 
 class ProfileSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = SmartMovingProfile
-        fields = ("birth_date", "sex", "type_movement")
+        fields = ("birth_date", "sex", "movement_type")
 
 
 class UserSerializer(serializers.ModelSerializer):
-    smartmovingprofile = ProfileSerializer()
+    smartmovingprofile = ProfileSerializer(required=False)
     password = serializers.CharField(
         write_only=True,
-        required=True,
+        required=False,
         help_text="Leave empty if no change needed",
-        style={"input_type": "password", "placeholder": "password"},
+        style={"input_type": "password", "placeholder": "Password"},
     )
 
     class Meta:
         model = User
-        # fields = "__all__"
-        fields = ("url", "username", "password", "smartmovingprofile")
+        fields = ("url", "username", "password", "first_name", "last_name", "email", "smartmovingprofile")
 
     def create(self, validated_data):
         profile_data = validated_data.pop("smartmovingprofile", None)
 
-
         password = validated_data.get("password")
         if password:
             validated_data["password"] = make_password(password)
-        print(profile_data)
-  
+
         with transaction.atomic():
             instance = User.objects.create(**validated_data)
             if profile_data:
                 SmartMovingProfile.objects.create(user=instance, **profile_data)
-                
+
         instance = User.objects.get(pk=instance.pk)
         return instance
 
@@ -60,12 +56,11 @@ class UserSerializer(serializers.ModelSerializer):
             if profile_data:
                 profile = instance.smartmovingprofile
                 if profile:
-
                     profile.birth_date = profile_data.get("birth_date", profile.birth_date)
                     profile.sex = profile_data.get("sex", profile.sex)
-                    #profile.type_movement = profile_data.get("type_movement", profile.type_movement)
-
+                    profile.type_movement = profile_data.get("type_movement", profile.movement_type)
                     profile.save()
+
                 else:
                     SmartMovingProfile.objects.create(user=instance, **profile_data)
 
